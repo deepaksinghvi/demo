7edc7c3a8727c9f0c690d08ff79ba5f662332e42
i333127 <deepak.singhvi@sap.com>
2020-03-15 17:12:40 +0530

    Updated with docker file
    Updated with docker file

--- src/main/java/com/demo/app/DemoApplication.java
+++ src/main/java/com/demo/app/DemoApplication.java
@@ -1,9 +1,11 @@
-package com.demo.demo;
+package com.demo.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"com.demo.app"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/demo/app/api/DemoController.java
+++ src/main/java/com/demo/app/api/DemoController.java
@@ -1,20 +1,27 @@
-package com.demo.demo.api;
+package com.demo.app.api;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class DemoController {
 
   @RequestMapping(value = "/demo", method = RequestMethod.GET)
   public ResponseEntity demo() {
-    return new ResponseEntity("Demo Controller", HttpStatus.OK);
+    log.info("Demo Controller ....");
+    log.info("===============\n");
+    return new ResponseEntity("Demo Controller\n", HttpStatus.OK);
+  }
+
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public ResponseEntity health(){
+    log.info("Demo Health Check ....");
+    return new ResponseEntity("health ok ", HttpStatus.OK);
   }
 
 }
